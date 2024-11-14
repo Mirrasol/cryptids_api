f68ce52741ec695d17eb1c246b9eba7a307dc059
Elena Dobruk <elena.dobruk@yandex.ru>
2024-11-14 17:10:19 +0300

    relocate web directory into main app dir, add new router Explorer

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from .web import explorer
 
 app = FastAPI()
 
+app.include_router(explorer.router)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "There!"}
+
+@app.get('/')
+def top():
+    return 'top here'
+
+
+@app.get('/echo/{thing}')
+def echo(thing):
+    return f'echoing {thing}'
