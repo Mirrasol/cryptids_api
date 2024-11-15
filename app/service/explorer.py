5afada7d33a61bd08901df4735fd85a3dbb5ad32
Elena Dobruk <elena.dobruk@yandex.ru>
2024-11-15 15:37:29 +0300

    add service level functions

---
+++ app/service/explorer.py
@@ -0,0 +1,26 @@
+from app.model.explorer import Explorer
+import app.fake.creature as data
+
+
+def get_all() -> list[Explorer]:
+    return data.get_all()
+
+
+def get_one(name: str) -> Explorer | None:
+    return data.get(name)
+
+
+def create(explorer: Explorer) -> Explorer:
+    return data.create(explorer)
+
+
+def replace(id, explorer: Explorer) -> Explorer:
+    return data.replace(id, explorer)
+
+
+def modify(id, explorer: Explorer) -> Explorer:
+    return data.modify(id, explorer)
+
+
+def delete(id, explorer: Explorer) -> bool:
+    return data.delete(id)
